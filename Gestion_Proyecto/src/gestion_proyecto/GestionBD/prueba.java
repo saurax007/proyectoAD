3dd0397d1ec7cc25cd506c7ce7470e2acf676fd0
Ion A <ionyo@users.noreply.github.com>
2016-11-15 17:06:49 -0800

    Master (#6)
    * inserts añadidos
    
    * arregladas varias cosas

---
+++ Gestion_Proyecto/src/gestion_proyecto/GestionBD/prueba.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestion_proyecto.GestionBD;
+
+/**
+ *
+ * @author ion
+ */
+public class prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CreacionBD bd = new CreacionBD();
+        Boolean flag = bd.ComprobarBD();
+        if (!flag) {
+            System.out.println("No hay bd creada"
+                    + "Creando..");
+            bd.CreacionBD();
+            System.out.println("Creada!");
+
+        } else {
+            System.out.println("Hay una bd creada, borrando...");
+            bd.BorrarBD();
+            System.out.println("Creando otra vez...");
+            bd.CreacionBD();
+        }
+    }
+
+}
